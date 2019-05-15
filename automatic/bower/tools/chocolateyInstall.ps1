11bf6861348b20a70166d49e6974c42842ee2b7e
Pascal Berger <pascal.berger@gmail.com>
2019-05-15 12:44:29 +0200

    (GH-53) Check if Node.js is installed

--- automatic/bower/tools/chocolateyInstall.ps1
+++ automatic/bower/tools/chocolateyInstall.ps1
@@ -2,4 +2,11 @@
 
 $version = '1.8.8'
 
-npm install -g bower@$version
+if (-Not (Get-Command "node" -errorAction SilentlyContinue)) {
+    Write-Error "Bower requires Node.js to be installed. To install with Chocolatey, use either of the commands below:"
+    Write-Error "  choco install nodejs"
+    Write-Error "  choco install nodejs-lts"
+    throw "Node.js not found"
+} else {
+    npm install -g bower@$version
+}
--- automatic/gulp-cli/tools/chocolateyInstall.ps1
+++ automatic/gulp-cli/tools/chocolateyInstall.ps1
@@ -2,4 +2,11 @@
 
 $version = '2.2.0'
 
-npm install -g gulp-cli@$version
+if (-Not (Get-Command "node" -errorAction SilentlyContinue)) {
+    Write-Error "Command line interface for gulp requires Node.js to be installed. To install with Chocolatey, use either of the commands below:"
+    Write-Error "  choco install nodejs"
+    Write-Error "  choco install nodejs-lts"
+    throw "Node.js not found"
+} else {
+    npm install -g gulp-cli@$version
+}
